6b3e9ef39c9f922dbd476416bbabf8cf8d38e56c
cl3v3rlink <cl3v3rlink@gmail.com>
2024-08-26 22:01:09 -0700

    week 3 attendence

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!')
+console.log("Hello from inside the main.js file");
+
+//let myVar;
+ 
+const myVar = -44;
+const myVarType = typeof(myVar)
+
+console.log("myVarType" + myVarType);
+console.log('myVarType ${myVarType}');
+
+
+
+
+function runNow() {
+    if(myVarType === "number") {
+        console.log('will 11 this one  run?');
+    }else{
+        console.log('will 13 line run?');
+    }
+}
+
+runNow()
+runNow()
