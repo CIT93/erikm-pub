9dfc6eea4848c6d6394590f65b74174556df2c70
cl3v3rlink <cl3v3rlink@gmail.com>
2024-08-29 23:49:11 -0700

    working code

--- w3/main.js
+++ w3/main.js
@@ -30,19 +30,19 @@ determineHouseHoldPts(4)
 // Ex for 4 members in the household
 numberInHousehold = 4;
 if (numberInHousehold === 1) {
-    carbonFootprintPoints += 14;
+    carbonFootprintPoints = carbonFootprintPoints + 14;
 } else if (numberInHousehold === 2) {
-    carbonFootprintPoints += 12;
+    carbonFootprintPoints = carbonFootprintPoints + 12;
 } else if (numberInHousehold === 3) {
-    carbonFootprintPoints += 10;
+    carbonFootprintPoints  = carbonFootprintPoints + 10;
 } else if (numberInHousehold === 4) {
-    carbonFootprintPoints += 8;
+    carbonFootprintPoints = carbonFootprintPoints + 8;
 } else if (numberInHousehold === 5) {
-    carbonFootprintPoints += 6;
+    carbonFootprintPoints = carbonFootprintPoints + 6;
 } else if (numberInHousehold === 6) {
-    carbonFootprintPoints += 4;
+    carbonFootprintPoints = carbonFootprintPoints = 4;
 } else if (numberInHousehold > 6) {
-    carbonFootprintPoints += 2;
+    carbonFootprintPoints = carbonFootprintPoints + 2;
 }
 
 console.log(`Based on the number of members in the household (${numberInHousehold}), the points would be ${carbonFootprintPoints}.`);
