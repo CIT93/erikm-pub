e83c5b095fa78fe0de15ac30609f2d674bbc0243
cl3v3rlink <cl3v3rlink@gmail.com>
2024-09-10 02:20:12 -0700

    update

--- w3/main.js
+++ w3/main.js
@@ -1,48 +1,43 @@
+function determineHouseSizePts (size) {
+  if (size === 'large') { 
+    carbonFootprintPoints = carbonFootprintPoints + 10
+  } 
+   else if (size === 'medium') { 
+    carbonFootprintPoints = carbonFootprintPoints + 7
+  } else if (size === 'small') { 
+    carbonFootprintPoints = carbonFootprintPoints + 4
+  } else if (size === 'apt') { 
+    carbonFootprintPoints = carbonFootprintPoints + 2
+  }
+
+}
+
+
 function determineHouseHoldPts(numberInHousehold) { 
     if (numberInHousehold === 1) { 
-      carbonFootprintPoints = carbonFootprintPoints + 14;
+      carbonFootprintPoints = carbonFootprintPoints + 14
     } else if (numberInHousehold === 2) { 
-      carbonFootprintPoints = carbonFootprintPoints + 12;
+      carbonFootprintPoints = carbonFootprintPoints + 12
     } else if (numberInHousehold === 3) { 
-      carbonFootprintPoints = carbonFootprintPoints + 10;
+      carbonFootprintPoints = carbonFootprintPoints + 10
     } else if (numberInHousehold === 4) { 
-      carbonFootprintPoints = carbonFootprintPoints + 8;
+      carbonFootprintPoints = carbonFootprintPoints + 8
     } else if (numberInHousehold === 5) { 
-      carbonFootprintPoints = carbonFootprintPoints + 6;
+      carbonFootprintPoints = carbonFootprintPoints + 6
     } else if (numberInHousehold === 6) { 
-      carbonFootprintPoints = carbonFootprintPoints + 4;
+      carbonFootprintPoints = carbonFootprintPoints + 4
     } else if (numberInHousehold > 6) { 
-      carbonFootprintPoints = carbonFootprintPoints + 2;
+      carbonFootprintPoints = carbonFootprintPoints + 2
     }
     console.log(`Based on the number of members in the household (${numberInHousehold}), the points would be ${carbonFootprintPoints}.`);
 }
 
 let carbonFootprintPoints = 0;
-//const numberInHousehold = 3; 
+ 
 
     
 // global scope 
 
 determineHouseHoldPts(3)
-determineHouseHoldPts(4)
 
-
-// Ex for 4 members in the household
-numberInHousehold = 4;
-if (numberInHousehold === 1) {
-    carbonFootprintPoints = carbonFootprintPoints + 14;
-} else if (numberInHousehold === 2) {
-    carbonFootprintPoints = carbonFootprintPoints + 12;
-} else if (numberInHousehold === 3) {
-    carbonFootprintPoints  = carbonFootprintPoints + 10;
-} else if (numberInHousehold === 4) {
-    carbonFootprintPoints = carbonFootprintPoints + 8;
-} else if (numberInHousehold === 5) {
-    carbonFootprintPoints = carbonFootprintPoints + 6;
-} else if (numberInHousehold === 6) {
-    carbonFootprintPoints = carbonFootprintPoints = 4;
-} else if (numberInHousehold > 6) {
-    carbonFootprintPoints = carbonFootprintPoints + 2;
-}
-
-console.log(`Based on the number of members in the household (${numberInHousehold}), the points would be ${carbonFootprintPoints}.`);
+determineHouseSizePts('apt')
